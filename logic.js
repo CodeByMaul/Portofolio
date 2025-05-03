6d73c2c1952dee72a3b2940db65e6f71b92e8283
unknown <msn.socialpreneur@gmail.com>
2025-05-03 19:45:03 +0700

    update link download via javascript/blob dan membuka halaman Detail Project Automasi Buku Kepegawaian
    merubah link downloadnya menggunakan javascript/blob, supaya nama file mudah dirubah,//, karena diganti Js/Blob, maka <a> diganti menjadi <span> supaya mudah, lalu diganti style nya.,//, mengupdate -Draft-Detail-Project.html dibagian download filenya.,//, Melengkapi Konten Halaman Automasi Buku Kepegawaian dan membuka Hidenya di 4portfolio.html.

--- logic.js
+++ logic.js
@@ -6,3 +6,17 @@ function toggleMenu() {
     navLinks.classList.toggle("show");
     hamburger.classList.toggle("active");
 }
+
+// Download File dengan nama baru :
+function downloadFile(url, filename) {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        });
+}
